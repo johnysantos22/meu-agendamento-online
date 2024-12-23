b185f1d4c4e110887ea9115182e549a5f9b50305
johnysantos22 <santosjohny302@gmail.com>
2024-12-23 15:18:47 -0300

    criando a tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 
 
-import './App.css'
+import './styles/Global.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Vite + React + TS</h1>
+      <div className="App">
+        <div className="card">
+        <img src="./src/assets/logo.svg" alt="" />
+          <h1>Agendamento Online</h1>
+
+          <a href="https://vitejs.dev" target="_blank">Agenda um horário</a>
+        </div>
+
       </div>
 
 
